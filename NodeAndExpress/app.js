ef9c4c609ac54176fc45755fb4c705e05e430d3b
nandanchakraborty <nandancsebubt@gmail.com>
2026-05-08 00:33:09 +0600

    updated

--- NodeAndExpress/app.js
+++ NodeAndExpress/app.js
@@ -5,8 +5,19 @@ const app = express();
 
 const PORT = 3000 || process.env.PORT;
 
+const mongoose = require('mongoose');
+mongoose
+    .connect('mongodb://localhost:27017/blogs')
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.log(err));
+    
+
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.locals.isActiveRoute = (route, currentRoute) =>
+    route === currentRoute ? 'active' : '';
 //templating engine
 
 app.use(expressLayout);
